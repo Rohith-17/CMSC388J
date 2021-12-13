feeb9b9be9316f935287be14fa2be379eb4bf920
Rohith-17 <32134244+Rohith-17@users.noreply.github.com>
2021-12-13 14:49:59 -0500

    Create models.py

---
+++ flask_app/models.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from . import db, login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(username=user_id).first()
+
+
+class User(db.Document, UserMixin):
+    username = db.StringField(required=True, unique=True)
+    email = db.EmailField(required=True, unique=True)
+    password = db.StringField(required=True)
+
+    def get_id(self):
+        return self.username
+
+class LandlordReview(db.Document, UserMixin):
+    author = db.StringField(required=True)
+    landlord_name = db.StringField(required=True)
+    location = db.StringField(required=True)
+    rating = db.IntField(required=True)
+    review_content = db.StringField(required=True)
+    landlord_id = db.StringField(required=True)
