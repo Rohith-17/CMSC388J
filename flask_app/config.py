44bde2c7ee524bc0e6b8d77c5a1c508280dde788
Rohith-17 <32134244+Rohith-17@users.noreply.github.com>
2021-12-13 19:00:40 -0500

    Update config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = "mongodb://localhost:27017/rate-my-umd-landlord"
+MONGODB_HOST = "mongodb+srv://rchandu:<Rohith17!>@cmsc388j-final.c8x5o.mongodb.net/CMSC388J-Final?retryWrites=true&w=majority"
